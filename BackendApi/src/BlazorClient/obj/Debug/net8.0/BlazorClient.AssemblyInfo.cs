48a61e375f8c2f8ea0d4b4b364be4bd822770cbe
miracorner <106338183+Mirahodec@users.noreply.github.com>
2025-10-29 20:35:50 +0300

    Add Cors
    2

--- BackendApi/src/BlazorClient/obj/Debug/net8.0/BlazorClient.AssemblyInfo.cs
+++ BackendApi/src/BlazorClient/obj/Debug/net8.0/BlazorClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c567c758976144883fed347d1750c82d2145438d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54449fbd7d4f2f74ed7b65dd97121336e9c8c0e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
